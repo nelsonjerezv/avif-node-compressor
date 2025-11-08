aa4f25661cfcb01d7fa3549ce81d97cb30a57042
Nelson Jerez <nelson.jerezv@gmail.com>
2025-11-07 23:33:18 -0300

    CHG: agrega watermark

--- compress.js
+++ compress.js
@@ -4,10 +4,14 @@ import sharp from 'sharp';
 
 const inputDir = './input';
 const outputDir = './output';
+const watermarkPath = './watermark.png'; // Asegúrate de que el watermark.png esté en la misma carpeta
 
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir);
 }
+
+// Lee el watermark una vez para reutilizarlo
+const watermarkBuffer = fs.readFileSync(watermarkPath);
 
 fs.readdir(inputDir, (err, files) => {
   if (err) {
@@ -23,9 +27,10 @@ fs.readdir(inputDir, (err, files) => {
 
     if (['.jpg', '.jpeg', '.png'].includes(ext)) {
       sharp(inputPath)
-        .avif({ quality: 25 }) // Puedes ajustar la calidad
+        .composite([{ input: watermarkBuffer, tile: true, blend: 'over', opacity: 0.3 }])
+        .avif({ quality: 25 })
         .toFile(outputPath)
-        .then(() => console.log(`Convertido: ${file} -> ${baseName}.avif`))
+        .then(() => console.log(`Convertido: ${file} -> ${baseName}.avif con watermark`))
         .catch(err => console.error(`Error al convertir ${file}:`, err));
     }
   });
